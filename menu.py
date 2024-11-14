5f67ee42ffe34cfb1d08127b7542a673496c0518
Marc Brodeur <46334979+mrbrodeur@users.noreply.github.com>
2024-11-14 15:14:58 -0500

    remove dataset types not dataset

--- menu.py
+++ menu.py
@@ -5,17 +5,19 @@ st.set_page_config(layout="wide")
 
 df = pd.read_json('data.json')
 df['view'] = '/view?id=' + df['id']
+df = df[df['dataset_type'].isin(['Dataset'])]
+df.sort_values('name', inplace=True)
 df_filtered = df
 row_count = len(df)
 categories = ['Transportation', 'City Government', 'Housing & Development',
  'Social Services', 'Public Safety', 'Health', 'Environment', 'Education',
  'Business', 'Recreation', 'NYC BigApps']
-dataset_types = ['Dataset', 'Map', 'Filtered View', 'File or Document', 'Story', 'External Link', 'Chart', None]
+# dataset_types = ['Dataset', 'Map', 'Filtered View', 'File or Document', 'Story', 'External Link', 'Chart', None]
 cat_selected = None
 type_selected = None
 dataframe_params = {
     'data': df_filtered,
-    'column_order': ['name', 'category', 'views', 'updated', 'dataset_type', 'view'],
+    'column_order': ['name', 'category', 'views', 'updated', 'view'],
     'column_config': {
         'view': st.column_config.LinkColumn('View', display_text='Explore'),
     },
@@ -30,7 +32,7 @@ st.markdown("This app is a visualizer for [NYC Open Data](http://opendata.cityof
 with st.container(border=True):
     st.header('Filters')
     cat_selected = st.pills('Filter by category...', categories)
-    type_selected = st.pills('Filter by dataset type...', dataset_types)
+    # type_selected = st.pills('Filter by dataset type...', dataset_types)
     text_filter = st.text_input("Filter by name")
 
     if st.button('Reset Filters'):
@@ -55,8 +57,8 @@ if text_filter:
     main_table.dataframe(**dataframe_params)
     counter_metric.metric(label='Number of datasets', value=len(dataframe_params['data']))
 
-if type_selected:
-    dataframe_params['data'] = df_filtered[df_filtered['dataset_type'] == type_selected]
-    main_table.dataframe(**dataframe_params)
-    counter_metric.metric(label='Number of datasets', value=len(dataframe_params['data']))
+# if type_selected:
+#     dataframe_params['data'] = df_filtered[df_filtered['dataset_type'] == type_selected]
+#     main_table.dataframe(**dataframe_params)
+#     counter_metric.metric(label='Number of datasets', value=len(dataframe_params['data']))
 
